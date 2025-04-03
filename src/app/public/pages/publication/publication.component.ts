6d57c6126b2324bb46c2d2ed7fa6e32e33225523
Jahir Celorio <jahir.celorio2001@gmail.com>
2025-04-03 16:44:27 -0500

    feat: :sparkles: installing primeng v19 for app

--- src/app/public/pages/publication/publication.component.ts
+++ src/app/public/pages/publication/publication.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-publication',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './publication.component.html',
   styleUrl: './publication.component.scss'
 })
