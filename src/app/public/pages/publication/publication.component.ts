f9eb3b0a30892da3ee4d4018c1bd6b0ccaee8623
Jahir Celorio <jahir.celorio2001@gmail.com>
2025-04-03 20:02:20 -0500

    fix: :bug: resolved color theme dark origin for app

--- src/app/public/pages/publication/publication.component.ts
+++ src/app/public/pages/publication/publication.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-
+import { ToggleSwitch } from 'primeng/toggleswitch';
 
 
 @Component({
   selector: 'app-publication',
-  imports: [ButtonModule],
+  imports: [ButtonModule, ToggleSwitch],
   templateUrl: './publication.component.html',
   styleUrl: './publication.component.scss',
 })
@@ -14,10 +14,49 @@ export class PublicationComponent {
 
   constructor() {}
 
-  toggleDarkMode(){
+
+
+  // Para cambiar al modo dark
+
+  toggleDarkMode() {
     const element = document.querySelector('html');
     if (element !== null) {
-      element.classList.toggle('my-app-dark')
+      element.classList.toggle('custom-dark-mode');
     }
   }
+
+
+  prueba(){
+    console.log('prueba');
+  }
+
+  amberSwitch = {
+    handle: {
+        borderRadius: '4px'
+    },
+    colorScheme: {
+        light: {
+            root: {
+                checkedBackground: '{amber.500}',
+                checkedHoverBackground: '{amber.600}',
+                borderRadius: '4px'
+            },
+            handle: {
+                checkedBackground: '{amber.50}',
+                checkedHoverBackground: '{amber.100}'
+            }
+        },
+        dark: {
+            root: {
+                checkedBackground: '{amber.400}',
+                checkedHoverBackground: '{amber.300}',
+                borderRadius: '4px'
+            },
+            handle: {
+                checkedBackground: '{amber.900}',
+                checkedHoverBackground: '{amber.800}'
+            }
+        }
+    }
+  };
 }
