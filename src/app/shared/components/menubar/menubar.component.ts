69a64202727170c4fe8c55b00cc38ef7bab9df8f
Jahir Celorio <jahir.celorio2001@gmail.com>
2025-04-05 19:46:27 -0500

    style: :lipstick: design added for options of user not authenticated

--- src/app/shared/components/menubar/menubar.component.ts
+++ src/app/shared/components/menubar/menubar.component.ts
@@ -4,16 +4,24 @@ import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Dialog } from 'primeng/dialog';
 
 
 @Component({
   selector: 'shared-menubar',
-  imports: [InputTextModule, ButtonModule, TooltipModule, CommonModule, FormsModule],
+  imports: [InputTextModule, ButtonModule, TooltipModule, CommonModule, FormsModule, Dialog],
   standalone: true,
   templateUrl: './menubar.component.html',
   styleUrl: './menubar.component.scss'
 })
 export class MenubarComponent {
   public searchQuery: string = '';
-  
+
+  public visible: boolean = false;
+
+
+  showDialog() {
+    this.visible = true;
+  }
+
 }
